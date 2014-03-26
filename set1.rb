70885acbc66123086c18a1c178680b24789d5a17
Wendy Hsu <wndyhsu@gmail.com>
2014-03-26 17:04:35 -0500

    completed the first problem

--- set1.rb
+++ set1.rb
@@ -1,8 +1,33 @@
 module Set1
   def self.swap_small(array)
+
+    if
+      array.length == 2
+     array.sort
+    elsif
+      array.length >2
+      first = array[0]
+      min = array.min
+      smallest_index = array.find_index(min)
+      array[smallest_index] =first
+      array[0] =min
+      array
+    else
+      array
+    end
   end
 
   def self.find_sum_2(array, sum = 0)
+    if
+      array.empty?
+      false
+    elsif
+      array.include?(0)
+      true
+    elsif
+      array.include?(num) && array.include?(-num)
+      true
+    end
   end
 
   def self.find_sum_3(array)
